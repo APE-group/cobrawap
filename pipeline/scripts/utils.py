7ff553cf2eda19fd63477ea26959f031da498793
Chiara De Luca <chdeluca18@gmail.com>
2021-11-23 14:00:14 +0100

    Allign to INM6

--- pipeline/scripts/utils.py
+++ pipeline/scripts/utils.py
@@ -10,6 +10,7 @@ import os
 import warnings
 import quantities as pq
 import matplotlib.pyplot as plt
+# from elephant.parallel import SingleProcess
 
 # def check_analogsignal_shape(asig):
 #     if type(asig) == list and len(asig) > 1:
@@ -225,12 +226,7 @@ def ImageSequence2AnalogSignal(block):
     for seg_count, segment in enumerate(block.segments):
         for imgseq in segment.imagesequences:
             dim_t, dim_x, dim_y = imgseq.as_array().shape
-            # coords = np.zeros((dim_x, dim_y, 2), dtype=int)
-            # for x, row in enumerate(coords):
-            #     for y, cell in enumerate(row):
-            #         coords[x][y][0] = x
-            #         coords[x][y][1] = y
-            # coords = coords.reshape((dim_x * dim_y, 2))
+
             coords = np.array(list(itertools.product(np.arange(dim_x),
                                                      np.arange(dim_y))))
 
@@ -445,3 +441,39 @@ def save_plot(filename):
     plt.savefig(fname=filename, bbox_inches='tight')
     plt.close()
     return None
+
+
+# class parallelize:
+#     """
+#     Context manager that applies elephant.parallel executors:
+#     ProcessPoolExecutor(), MPIPoolExecutor(), MPICommExecutor(), or
+#     SingleProcess() (default).
+#
+#     Example:
+#     ```
+#     results = [my_function(arg) for arg in iterables_list]
+#     ```
+#     becomes
+#     ```
+#     with parallelize(my_function) as parallel_func:
+#         results = parallel_func(iterables_list, *args, **kwargs)
+#     ```
+#     """
+#     def __init__(self, func, executor=SingleProcess()):
+#         self.executor = executor
+#         self.func = func
+#
+#     def __enter__(self):
+#
+#         def _func(iterable, *args, **kwargs):
+#
+#             def arg_func(iterable):
+#                 return self.func(iterable, *args, **kwargs)
+#
+#             result = self.executor.execute(arg_func, iterable)
+#             return result
+#
+#         return _func
+#
+#     def __exit__(self, exc_type, exc_value, exc_traceback):
+#         pass
