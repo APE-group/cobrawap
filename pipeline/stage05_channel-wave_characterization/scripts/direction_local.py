152eba2cc588a14ef9376d178bda0622937297f3
Chiara De Luca <chdeluca18@gmail.com>
2022-02-27 12:00:27 +0100

    Local directions on macro-pixels
    local directions are computed on a donwampled image to avoid
    noisy measurements

--- pipeline/stage05_channel-wave_characterization/scripts/direction_local.py
+++ pipeline/stage05_channel-wave_characterization/scripts/direction_local.py
@@ -29,11 +29,25 @@ if __name__ == '__main__':
     direction_df['direction_local_x'] = df.dt_x
     direction_df['direction_local_y'] = df.dt_y
     direction_df[f'{args.event_name}_id'] = df[f'{args.event_name}_id']
+    
+    # code to make directions less noisy
+    x_reduced = np.array(df.x_coords // 6)
+    y_reduced = np.array(df.y_coords // 6)
+    dt_x_red = []
+    dt_y_red = []
+
+    for x in np.unique(x_reduced):
+        for y in np.unique(y_reduced):
+            idx = np.intersect1d(np.where(x_reduced == x)[0], np.where(y_reduced == y)[0])
+            dt_x_red.append(np.mean(np.array(df.dt_x)[idx]))
+            dt_y_red.append(np.mean(np.array(df.dt_y)[idx]))
 
     direction_df.to_csv(args.output)
 
     fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
-    ax.hist(np.angle(df.dt_x*1j + df.dt_y), bins=36, range=[-np.pi, np.pi])
+    #ax.hist(np.angle(df.dt_x*1j + df.dt_y), bins=36, range=[-np.pi, np.pi])
+    #ax.hist(np.arctan2(1./df.dt_y,  1./df.dt_x), bins=36, range=[-np.pi, np.pi])
+    ax.hist(np.arctan2(1./np.array(dt_y_red),  1./np.array(dt_x_red)), bins=36, range=[-np.pi, np.pi])
 
     if args.output_img is not None:
         save_plot(args.output_img)
