19941d8701bfe39ed152ee355e4bd4967599ee6a
cosimolupo <cosimo.lupo89@gmail.com>
2024-02-01 17:36:56 +0100

    Sync APE/master with NeuralEnsemble/master: stage01 changes

--- cobrawap/pipeline/stage01_data_entry/scripts/check_input.py
+++ cobrawap/pipeline/stage01_data_entry/scripts/check_input.py
@@ -1,19 +1,21 @@
 """
+Check whether the input data representation adheres to the stage's requirements.
 
+Additionally prints a short summary of the data attributes.
 """
+
 import numpy as np
 import argparse
-import quantities as pq
-from utils.io import load_neo
-from utils.neo_utils import analogsignals_to_imagesequences, imagesequences_to_analogsignals
+from pathlib import Path
+from utils.io_utils import load_neo
+from utils.neo_utils import analogsignal_to_imagesequence, imagesequence_to_analogsignal
 
+CLI = argparse.ArgumentParser()
+CLI.add_argument("--data", nargs='?', type=Path, required=True,
+                 help="path to input data in neo format")
 
 if __name__ == '__main__':
-    CLI = argparse.ArgumentParser(description=__doc__,
-                   formatter_class=argparse.RawDescriptionHelpFormatter)
-    CLI.add_argument("--data", nargs='?', type=str, required=True,
-                     help="path to input data in neo format")
-    args = CLI.parse_args()
+    args, unknown = CLI.parse_known_args()
 
     block = load_neo(args.data)
 
@@ -26,16 +28,14 @@ if __name__ == '__main__':
 
     asig = block.segments[0].analogsignals[0]
 
-    block = analogsignals_to_imagesequences(block)
-    block = imagesequences_to_analogsignals(block)
-    asig2 = block.segments[0].analogsignals[1]
+    imgseq = analogsignal_to_imagesequence(asig)
+    asig2 = imagesequence_to_analogsignal(imgseq)
 
     if asig.shape != asig2.shape:
         raise ValueError("AnalogSignal doesn't include empty grid sites. "
                       + f"Reshape {asig.shape} to {asig2.shape} according to "
                          "x/y_coords. You may use `add_empty_sites_to_analogsignal` "
                          "from the utils.neo_utils module.")
-    del asig2
 
     print('Recording Time:\t\t', asig.t_stop - asig.t_start)
     print('Sampling Rate:\t\t', asig.sampling_rate)
@@ -46,6 +46,8 @@ if __name__ == '__main__':
 
     x_coords = asig.array_annotations['x_coords']
     y_coords = asig.array_annotations['y_coords']
+
     dim_x, dim_y = np.max(x_coords)+1, np.max(y_coords)+1
+
     print('Grid Dimensions:\t', f'{dim_x} x {dim_y}')
     print('Empty Grid Sites:\t', dim_x*dim_y - num_channels)
--- cobrawap/pipeline/stage01_data_entry/scripts/plot_traces.py
+++ cobrawap/pipeline/stage01_data_entry/scripts/plot_traces.py
@@ -1,24 +1,27 @@
 """
-Plot traces
------------
-
-description...
-
-Input: neo.Block with ...
-
-Output: neo.Block + ...
-
+Plots excerpts of the input data with its corresponding metadata.
 """
+
 import numpy as np
 import argparse
+from pathlib import Path
 import matplotlib.pyplot as plt
 import seaborn as sns
-import quantities as pq
-import random
-from utils.io import load_neo, save_plot
+from utils.io_utils import load_neo, save_plot
 from utils.neo_utils import time_slice
-from utils.parse import parse_plot_channels, none_or_int, none_or_float, determine_dims
+from utils.parse import parse_plot_channels, none_or_int, none_or_float
 
+CLI = argparse.ArgumentParser()
+CLI.add_argument("--data",    nargs='?', type=Path, required=True,
+                    help="path to input data in neo format")
+CLI.add_argument("--output",  nargs='?', type=Path, required=True,
+                    help="path of output figure")
+CLI.add_argument("--t_start", nargs='?', type=none_or_float, default=0,
+                    help="start time in seconds")
+CLI.add_argument("--t_stop",  nargs='?', type=none_or_float, default=10,
+                    help="stop time in seconds")
+CLI.add_argument("--channels", nargs='+', type=none_or_int, default=0,
+                    help="list of channels to plot")
 
 def plot_traces(asig, channels):
     sns.set(style='ticks', palette="deep", context="notebook")
@@ -34,15 +37,16 @@ def plot_traces(asig, channels):
     array_annotations = [f'{k}: {v[channels]}'
                         for k,v in asig.array_annotations.items()]
 
-    coords = np.stack((asig.array_annotations['x_coords'],
-                       asig.array_annotations['y_coords']), axis=-1)
+    x_coords = asig.array_annotations['x_coords']
+    y_coords = asig.array_annotations['y_coords']
+    dim_x, dim_y = np.max(x_coords)+1, np.max(y_coords)+1
 
     ax.text(ax.get_xlim()[1]*1.05, ax.get_ylim()[0],
             f'ANNOTATIONS FOR CHANNEL(s) {channels} \n'\
           +  '\n ANNOTATIONS:\n' + '\n'.join(annotations) \
           +  '\n\n ARRAY ANNOTATIONS:\n' + '\n'.join(array_annotations) +'\n' \
           + f' t_start: {asig.t_start}; t_stop: {asig.t_stop} \n' \
-          + f' dimensions(x,y): {determine_dims(coords)}')
+          + f' dimensions(x,y): {dim_x}, {dim_y}')
 
     ax.set_xlabel(f'time [{asig.times.units.dimensionality.string}]')
     ax.set_ylabel(f'channels [in {asig.units.dimensionality.string}]')
@@ -52,18 +56,6 @@ def plot_traces(asig, channels):
 
 
 if __name__ == '__main__':
-    CLI = argparse.ArgumentParser(description=__doc__,
-                   formatter_class=argparse.RawDescriptionHelpFormatter)
-    CLI.add_argument("--data",    nargs='?', type=str, required=True,
-                     help="path to input data in neo format")
-    CLI.add_argument("--output",  nargs='?', type=str, required=True,
-                     help="path of output figure")
-    CLI.add_argument("--t_start", nargs='?', type=none_or_float, default=0,
-                     help="start time in seconds")
-    CLI.add_argument("--t_stop",  nargs='?', type=none_or_float, default=10,
-                     help="stop time in seconds")
-    CLI.add_argument("--channels", nargs='+', type=none_or_int, default=0,
-                     help="list of channels to plot")
     args, unknown = CLI.parse_known_args()
 
     asig = load_neo(args.data, 'analogsignal', lazy=True)
