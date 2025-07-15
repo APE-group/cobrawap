94e0b729587e2cbd0db9c99c8faa5d5d866e2b9a
Cosimo Lupo <cosimo.lupo89@gmail.com>
2025-07-15 19:04:56 +0200

    Alignment changes

--- cobrawap/pipeline/stage02_processing/scripts/convolve.py
+++ cobrawap/pipeline/stage02_processing/scripts/convolve.py
@@ -16,45 +16,42 @@ CLI.add_argument("--data", nargs='?', type=Path, required=True,
 CLI.add_argument("--output", nargs='?', type=Path, required=True,
                  help="path of output file")
 CLI.add_argument("--kernel_type", nargs='?', type=str, required=True,
-                 help="type of convolution kernel")
-CLI.add_argument("--kernel_params", nargs='+', type=str, default=None,
+                 choices=["gaussian"], help="type of convolution kernel")
+CLI.add_argument("--kernel_params", nargs='+', type=str, required=True,
                  help="parameters of convolution kernel")
 
 
 def gaussian_kernel(duration_ms, sampling_rate_Hz, std_dev_ms):
     """
     Generates a Gaussian kernel for smoothing.
-    
+
     Parameters:
     - duration_ms: The duration over which the kernel is non-zero.
     - sampling_rate_Hz: The number of samples per second.
     - std_dev_ms: The standard deviation of the Gaussian kernel in ms.
-    
+
     Returns:
     - Gaussian kernel array.
     """
-    sampling_duration_s = 1 / sampling_rate_Hz
-    sampling_duration_ms = sampling_duration_s * 1000.0  # ms / s
-    kernel_start_ms = -duration_ms / 2.0
-    kernel_stop_ms = duration_ms / 2.0
-    #print("kernel_start_ms", kernel_start_ms)
-    #print("kernel_stop_ms", kernel_stop_ms)
-    #print("sampling_duration_ms", sampling_duration_ms)
+    sampling_duration_s = 1/sampling_rate_Hz
+    sampling_duration_ms = sampling_duration_s*1000.0
+    kernel_start_ms = -0.5*duration_ms
+    kernel_stop_ms = 0.5*duration_ms
     t_ms = np.arange(kernel_start_ms, kernel_stop_ms, sampling_duration_ms)
-    #print("length in samples", len(t_ms))
-    gaussian = np.exp(-0.5 * (t_ms / std_dev_ms) ** 2)
-    gaussian /= gaussian.sum()  # Normalize the kernel to ensure the signal energy is preserved
+    gaussian = np.exp(-0.5*(t_ms/std_dev_ms)**2)
+    # Normalize the kernel to ensure the signal energy is preserved
+    gaussian /= gaussian.sum()
     return gaussian
 
 
 def smooth_signal(asig, kernel_type, kernel_params):
     """
     Smooths the given signal by convolving it with the specified kernel.
-    
+
     Parameters:
-    - signal: The original signal 
+    - signal: The original signal.
     - kernel: The smoothing kernel.
-    
+
     Returns:
     - Smoothed signal.
     """
@@ -69,7 +66,7 @@ def smooth_signal(asig, kernel_type, kernel_params):
 
     convolved_signals = np.empty(asig.shape)
     convolved_signals.fill(np.nan)
-    
+
     for channel in range(asig.shape[1]):
         channel_signal = asig.as_array()[:,channel]
         if np.isnan(channel_signal).any():
@@ -79,7 +76,7 @@ def smooth_signal(asig, kernel_type, kernel_params):
 
     convolved_asig = asig.duplicate_with_new_data(convolved_signals)
     convolved_asig.array_annotate(**asig.array_annotations)
-    
+
     return convolved_asig
 
 
@@ -95,6 +92,5 @@ if __name__ == '__main__':
     convolved_asig.description += "Convolved with a {} kernel of parameters {} ({}). "\
                         .format(args.kernel_type, args.kernel_params, os.path.basename(__file__))
     block.segments[0].analogsignals[0] = convolved_asig
-    print(np.sum(asig[100:-100,221]),np.sum(convolved_asig[100:-100,221]))
 
     write_neo(args.output, block)
